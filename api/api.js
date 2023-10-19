5648f9f57a348d04eef45158f2a1f5507992a551
nicolastuomaala <nicolas.tuomaala00@gmail.com>
2023-10-19 16:09:34 +0300

    api/api.js

--- api/api.js
+++ api/api.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const cors = require('cors');
-const { convertTextToSpeech, fetchAvailableVoices } = require('./src/convert');
+const { convertTextToSpeech, fetchAvailableVoices } = require('../src/convert');
 const app = express();
 const port = 3001;
 
